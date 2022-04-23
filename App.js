b555012820b1ae62ee71f8639e44e0a873d300f2
Allan Torres <allaneduardot@gmail.com>
2022-04-23 02:15:27 -0400

    Cards Css commit

--- App.js
+++ App.js
@@ -20,9 +20,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
-    width:'100%',
+    width:'100%',height:1500
   },
 });
--- components/DisplayVideoGames.js
+++ components/DisplayVideoGames.js
@@ -8,28 +8,31 @@ export const DisplayVideoGames = ({VideoGames}) => {
   return (
     
        
-      <ScrollView style={{width:'100%'}}>
+      <ScrollView style={{width:'100%',top:10}}>
           {
    
            VideoGames.slice(0,9).map((e,i)=>{
         
           return( 
             <Button
-            title={<CustomGame name={e.name} i={i}/>}          
+            title={<CustomGame name={e.name} i={i} genre={e.genres[0].name}/>}          
             buttonStyle={styles.button}
             onPress={() =>
               navigation.navigate('Hvac')
             }   
             key={i} 
-    />
+            />
+            
             )
-          })          
+          })    
+         
           }
+            
         </ScrollView>  
    
   )
 }
-const CustomGame = ({name,i}) => {
+const CustomGame = ({name,i,genre}) => {
   return (
     <View style={{ 
       flex:1,
@@ -38,23 +41,24 @@ const CustomGame = ({name,i}) => {
       backgroundColor:'transparent',  
       width:'100%'    
   }}>
-       <View style={{backgroundColor:'transparent',width:'50%' }}>
-          <Text style={styles.letterButton}>{name}</Text>
+       <View style={{backgroundColor:'transparent',width:'60%' }}>
+          <Text style={styles.letterButton}>{name}</Text> 
+          <Text style={styles.letterButton_}> genre: {genre}</Text>
        </View>
-       <View style={{backgroundColor:'transparent',width:'50%',height:93,left:0}}>  
-       {/* <Image style={{width:100,height:100,right:0,borderRadius: 10, }} source={{uri :'https://media.rawg.io/media/resize/200/-/screenshots/86e/86e4ab903e6352fc1c2451a1402a0ad1.jpg'}} />   */}
+       
+       <View style={{backgroundColor:'transparent',width:'50%',height:93,left:'50%',top:30}}>  
 {           
-  i===0?( <Image style={{width:100,height:100,right:0,borderRadius: 10, }} source={{uri :'https://media.rawg.io/media/resize/200/-/screenshots/a7c/a7c43871a54bed6573a6a429451564ef.jpg'}} />)
- :i===1?(<Image style={{width:100,height:100,right:0,borderRadius: 10, }} source={{uri :'https://media.rawg.io/media/resize/200/-/screenshots/1ac/1ac19f31974314855ad7be266adeb500.jpg'}} /> )
- :i===2?(<Image style={{width:100,height:100,right:0,borderRadius: 10, }} source={{uri :'https://media.rawg.io/media/resize/200/-/screenshots/b11/b11a2ae0664f0e8a1ef2346f99df26e1.jpg'}} />)
- :i===3?(<Image style={{width:100,height:100,right:0,borderRadius: 10, }} source={{uri :'https://media.rawg.io/media/resize/200/-/screenshots/4f9/4f9d5efdecfb63cb99f1baa4c0ceb3bf.jpg'}} />)
- :i===4?(<Image style={{width:100,height:100,right:0,borderRadius: 10, }} source={{uri :'https://media.rawg.io/media/resize/200/-/screenshots/3bd/3bd2710bd1ffb6664fdea7b83afd067e.jpg'}} />)
- :i===5?(<Image style={{width:100,height:100,right:0,borderRadius: 10, }}  source={{uri :'https://media.rawg.io/media/resize/200/-/screenshots/44c/44c688a8122f4e31cf85c219ab790075.jpg'}} />)
- :i===6?(<Image style={{width:100,height:100,right:0,borderRadius: 10, }} source={{uri :'https://media.rawg.io/media/resize/200/-/screenshots/ff1/ff16661bb15f7969b44f6c4118870e44.jpg'}} />)
- :i===7?(<Image style={{width:100,height:100,right:0,borderRadius: 10, }} source={{uri :'https://media.rawg.io/media/resize/200/-/screenshots/99e/99e94bd55eb75fa6e75c3dcbb1a570b2.jpg'}} />)
- :i===8?(<Image style={{width:100,height:100,right:0,borderRadius: 10, }} source={{uri :'https://media.rawg.io/media/resize/200/-/screenshots/bf0/bf07e2c6d2c888d372917d9ef453c8a4.jpg'}} />)
- :i===9?(<Image style={{width:100,height:100,right:0,borderRadius: 10, }} source={{uri :'https://media.rawg.io/media/resize/200/-/screenshots/616/61643dd96e936d29eb68cf53b2334e53.jpg'}} />)
- :i===10?(<Image style={{width:100,height:100,right:0,borderRadius: 10, }} source={{uri :'https://media.rawg.io/media/resize/200/-/screenshots/86e/86e4ab903e6352fc1c2451a1402a0ad1.jpg'}} />)
+  i===0?( <Image style={styles.img} source={{uri :'https://media.rawg.io/media/resize/200/-/screenshots/a7c/a7c43871a54bed6573a6a429451564ef.jpg'}} />)
+ :i===1?(<Image style={styles.img} source={{uri :'https://media.rawg.io/media/resize/200/-/screenshots/1ac/1ac19f31974314855ad7be266adeb500.jpg'}} /> )
+ :i===2?(<Image style={styles.img} source={{uri :'https://media.rawg.io/media/resize/200/-/screenshots/b11/b11a2ae0664f0e8a1ef2346f99df26e1.jpg'}} />)
+ :i===3?(<Image style={styles.img} source={{uri :'https://media.rawg.io/media/resize/200/-/screenshots/4f9/4f9d5efdecfb63cb99f1baa4c0ceb3bf.jpg'}} />)
+ :i===4?(<Image style={styles.img} source={{uri :'https://media.rawg.io/media/resize/200/-/screenshots/3bd/3bd2710bd1ffb6664fdea7b83afd067e.jpg'}} />)
+ :i===5?(<Image style={styles.img}  source={{uri :'https://media.rawg.io/media/resize/200/-/screenshots/44c/44c688a8122f4e31cf85c219ab790075.jpg'}} />)
+ :i===6?(<Image style={styles.img} source={{uri :'https://media.rawg.io/media/resize/200/-/screenshots/ff1/ff16661bb15f7969b44f6c4118870e44.jpg'}} />)
+ :i===7?(<Image style={styles.img} source={{uri :'https://media.rawg.io/media/resize/200/-/screenshots/99e/99e94bd55eb75fa6e75c3dcbb1a570b2.jpg'}} />)
+ :i===8?(<Image style={styles.img} source={{uri :'https://media.rawg.io/media/resize/200/-/screenshots/bf0/bf07e2c6d2c888d372917d9ef453c8a4.jpg'}} />)
+ :i===9?(<Image style={styles.img} source={{uri :'https://media.rawg.io/media/resize/200/-/screenshots/616/61643dd96e936d29eb68cf53b2334e53.jpg'}} />)
+ :i===10?(<Image style={styles.img} source={{uri :'https://media.rawg.io/media/resize/200/-/screenshots/86e/86e4ab903e6352fc1c2451a1402a0ad1.jpg'}} />)
  :<></>
       }
        </View>     
@@ -63,6 +67,7 @@ const CustomGame = ({name,i}) => {
   };
 
 const styles = StyleSheet.create({
+  img:{width:110,height:100,right:0,borderRadius: 10, },
   container: {
     flex: 1,
     backgroundColor: '#fff',
@@ -77,23 +82,39 @@ const styles = StyleSheet.create({
   },
     
   button:{
-    borderWidth: 0,
-    borderColor: 'transparent',
+    borderWidth: 1,
+    borderColor: '#EEA111',
     borderRadius: 10,
     width: '100%',
     height: 120,
       marginTop:10,
       marginBottom:10,
       marginLeft:0,
-      backgroundColor:'#1B61AA',
+      backgroundColor:'#6C7592',
      
   },
   letterButton:{
   
     color:'white',
-    fontSize: 30,
-  marginTop:'9%',
+    fontSize:25,
+  marginTop:25,
     letterSpacing: -0.24,
-     height:50, width: '100%',
+     height:60, width: '100%',
+     borderBottomWidth:1
+  },
+  letterButton_:{
+  
+    color:'white',
+    fontSize:20,
+  marginTop:15,
+    letterSpacing: -0.24,
+     height:60, width: '100%',
+     borderBottomWidth:2,
+     paddingLeft:30,
+    paddingRight:30,
+    textShadowColor:'black',
+    textShadowOffset:{width: 5, height: 5},
+    textShadowRadius:10,
+
   },
 });
